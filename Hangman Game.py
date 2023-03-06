1bc17e7316a05059a750ad84560f4bd313ccb4cb
IntuitiveOne <ituition@live.co.uk>
2023-03-06 23:40:41 +0200

    updating and adding projects/exercises

--- Hangman Game.py
+++ Hangman Game.py
@@ -15,24 +15,16 @@ def hangmangame():
     wordnik_service = Wordnik()                                                            #defining function of Wordnik which is an online dictionary that a word is selected from when called using the random_word libary
 
     while definition_results is None or "-" in newhangmanword or " " in newhangmanword:    #loop to select different words from Wordnik Libary until word found Pydictionary can lookup a definition for (PyDictionary does not work with words with spaces etc)
-        newhangmanword  = "Africa" #wordnik_service.get_random_word(minLength=5)                     #using random_word libary and Wordnik to select a random word 
-        definition_results = (dictionary.meaning(newhangmanword, disable_errors=True))
+        newhangmanword  = wordnik_service.get_random_word(minLength=5)                     #using random_word libary and Wordnik to select a random word of at least 5 letters of length 
+        definition_results = (dictionary.meaning(newhangmanword, disable_errors=True))     #Using Pydictionary definitions function to allow the user the option of asking for a definition of the randomized word
         
 
-    propernoun = 0
+    propernoun = 0                                                           #variable that will be used for words with capital letters etc
 
     wordinlist = list(newhangmanword)                                        #creating list from the word to guess for easier variable manipulations
 
-    if wordinlist[0].isupper() is True:
+    if wordinlist[0].isupper() is True:                                      #determining if the random word is a propernoun or not
         propernoun = 1
-
-
-    print(propernoun)    
-
-    print(newhangmanword)
-    print(wordinlist)
-
-
 
     wordsize = int(len(newhangmanword))
     revealletterslist = ["_"] * wordsize                                     #creating correctly sized reveallist to populate with letter guesses of user
@@ -145,8 +137,12 @@ def hangmangame():
     alphabet = list(string.ascii_letters)                                            #list for if statements checking if letter guesses are appropiate inputs or not
     punctuation = list(string.punctuation)                                           #list for if statements checking if word guesses have inappriopiate punctuation in(the word guess must only be letters)
 
-    while lives < 7: 
+    win = ""                                                                         #variable used to determine game ending message       
+    guessproper = ""
+    guesscapitalletter = ""
 
+    while lives < 7: 
+        
         guessedlettersstringconvert = [str(x) for (x) in revealletterslist]          #converting the lists into strings every parse to be outputted for better user experience(i.e. "_ _ _ _" not "[ _, _, _, _ ]")
         guessedletters = ""
         guessedletters = guessedletters.join(guessedlettersstringconvert)
@@ -157,64 +153,76 @@ def hangmangame():
         usedletters = usedletters.join(usedlettersstringconvert)
         usedletters = "  ".join(usedletters)
 
+        usedwordstringconvert = [str(x) for (x) in usedwordlist]                
+        usedwords = " "
+        usedwords = usedwords.join(usedwordstringconvert)
+        usedwords = "".join(usedwords)
 
-        pcount = 0
-        guessproper = ""
+        pcount = 0                                                                  #variable used when checking if incorrect characters were used in user's guess
+       
 
-
-        guess = input(f"{hangman[lives]} \n\nYou have {livesleft} lives left, gusss the word or a letter, choose wisely!\n\n {guessedletters}\n\nThese letters have already been guessed   {usedletters}\n\nThese words have already been guessed {usedwordlist}\n\n ").lower()
+        guess = input(f"{hangman[lives]} \n\nYou have {livesleft} lives left, gusss the word or a letter, choose wisely!\n\nThe word is {wordsize} letters long.\n\n {guessedletters}\n\nThese letters have already been guessed: {usedletters}\n\nThese words have already been guessed: {usedwords}\n\n").lower()
         guessinlist = list(guess)
-        print(guessinlist[0])       
-        if propernoun == 1 and len(guess) > 1:          
+
+        if propernoun == 1 and len(guess) > 1:                                      #propernoun and capital letter conversions used when a random proper noun is the generated word
             capitalconvert = guessinlist[0]
             capitalconvert = capitalconvert.upper()
             guessinlist[0] = capitalconvert
-            guessproper = "".join(guessinlist)
-            print(guess)
+            guessproper = "".join(guessinlist)        
         elif propernoun == 1 and len(guess) <2:
             guesscapitalletter = guess.upper()     
         
+        
         spellcheck = Word(guess)
-        spellingresult = spellcheck.spellcheck()                                    #used in if statement checking if the user spell their guess correctly
+        spellingresult = spellcheck.spellcheck()                                    #used in if statement checking if the user spelled their guess correctly
         spellcheckstringconvert = [str(x) for (x) in spellingresult[0]]
         spellchecks = ""
         spellchecks = spellchecks.join(spellcheckstringconvert)
         spellchecks = "".join(spellchecks)
 
-        print(spellchecks)
         for i in range (0, len (guess)):                                            #counter for characters in guesses that aren't letters(used in if statements enforcing guesses only use letters and no other characters)
             if guess[i] in punctuation:  
-                pcount = pcount + 1  
-        if len(guess) < 2 and guess in wordinlist:  #if user guesses singular letter(s) that is(are) in the word
+                pcount = pcount + 1 
+
+        if guess == newhangmanword or guessproper == newhangmanword or "_" not in revealletterslist:                #if user guesses the word, this also takes into account the possibility of the word being a proper noun
+            print(f"\n\n{newhangmanword} is the correct word!, you have escaped the hangman for now...\n\n")
+            lives = 8
+            win = "y"           
+        elif guess not in alphabet and len(guess) <2 or pcount > 0:                 #if user gives an inappropiate input (not a letter or a word containing anything besides letters)
+            print("\n\nYou have not provided a letter or word, please try again.\n\n")
+        elif guess+"1.0" not in spellchecks:                                        #if user gives a word guess that is spelled incorrectly
+            print("The word you have guessed is either spelled incorectly or not in the dictionary used's possible words, please try again.\n\n")
+        elif len(guess) != wordsize and len(guess) > 1:                             #if user guesses a word of incorrect length
+            print("The word you guessed does not have the correct amount of letters, please try again.\n\n")      
+        elif len(guess) < 2 and guess in wordinlist and guess not in usedletterlist:#if user guesses singular letter(s) that is(are) in the word
             livesleft = livesleft -1       
             lives = lives +1
-            usedletterlist.append(guess)
+            usedletterlist.append(guess)           
             indices = [i for i in range(wordsize) if wordinlist[i] == guess]
             while len(indices) > 0:                                                 #seperating Indexes of guessed letters within the correct word and manipulating to allow duplicate letter placement within the reveal letter list    
                 revealletterslist[indices[0]] = guess                               #using loop with a created list of the index values that is parsed through deleting items from the list of index values with pop function to make sure letters go to indexes of reveal letter list correctly without overlap
-                indices.pop(0)        
-        elif guess == newhangmanword or guessproper == newhangmanword:                                               #if user guesses the word
-            print(f"\n\n{newhangmanword} is the correct word!, you have escaped the hangman for now...\n\n")
-            lives = 8
-            exit()
-        elif len(guess) == 1 and guess in usedletterlist:                           #if user guesses a letter already guessed
-            print("\n\nYou have already guessed this letter") 
-        elif len(guess) > 1 and guess in usedwordlist:                              #if user guesses a word already guessed
-            print("\n\nYou have already guessed this word")      
-        elif guess not in alphabet and len(guess) <2 or pcount > 0:                 #if user gives an inappropiate input (not a letter or a word containing anything besides letters)
-            print("\n\nYou have not provided a letter or word, please try again 1")
-        elif guess+"1.0" not in spellchecks:                                        #if user gives a word guess that is spelled incorrectly
-            print("The word you have guessed is spelled incorectly, please try again")
-        elif len(guess) == 1:                                                       #if user guesses a letter not in the word
+                indices.pop(0)
+            indicescapital = [i for i in range(wordsize) if wordinlist[i] == guesscapitalletter]
+            while len(indicescapital) > 0:                                          #seperating Indexes of guessed letters within the correct word and manipulating to allow duplicate letter placement within the reveal letter list    
+                revealletterslist[indicescapital[0]] = guesscapitalletter           #using loop with a created list of the index values that is parsed through deleting items from the list of index values with pop function to make sure letters go to indexes of reveal letter list correctly without overlap
+                indicescapital.pop(0)     
+        elif len(guess) == 1 and guess not in usedletterlist:                       #if user guesses a letter not in the word
             livesleft = livesleft -1  
             lives = lives +1        
             usedletterlist.append(guess)
-        elif len(guess) >1 and guess not in usedwordlist:                           #if user guesses an incorrect word
+        elif len(guess) == wordsize and guess not in usedwordlist:                  #if user guesses an incorrect word
             livesleft = livesleft -1  
             lives = lives +1  
             usedwordlist.append(guess)  
+        elif len(guess) == 1 and guess in usedletterlist:                           #if user guesses a letter already guessed
+            print("\n\nYou have already guessed this letter.")                
+        elif len(guess) == wordsize and guess in usedwordlist:                      #if user guesses a word already guessed
+            print("\n\nYou have already guessed this word.") 
 
-    askdef = input(f"\n\n {hangman[7]}\n\nYou could not guess the word, it is you for who the bell tolls, the word was {newhangmanword}\n\n Would you like the definition of the word? y/n").lower()
+    askdef = ""
+
+    if win != "y":
+        askdef = input(f"\n\n {hangman[7]}\n\nYou could not guess the word, it is you for who the bell tolls, the word was {newhangmanword}\n\nWould you like the definition of the word? y/n\n\n").lower()
 
    
 
@@ -222,10 +230,12 @@ def hangmangame():
     while askdef != "n": 
 
         if askdef == "y":
+            print("\n\n")
             print(definition_results)
+            print("\n\n")
             break            
         else: 
-            askdef = input("Would you like the definition of the word? y/n").lower()
+            askdef = input("Would you like the definition of the word? y/n\n\n").lower()
 
     playagain = ""
 
@@ -235,11 +245,10 @@ def hangmangame():
         elif playagain == "n":
             exit()
         else:
-            playagain = input("Would you like to play again? y/n").lower()                                                                                  
+            playagain = input("Would you like to play again? y/n\n\n").lower()                                                                                  
 
 hangmangame()
 
                                                                                     #Things to Add
-                                                                                    #Get API Key from Wordnik(to use Wordnik definitions alongside the random word generation -> increase amount of word possibilities including hypenations and Proper Nouns etc)                                                                      
-                                                                                    #Proper Nouns Logic? Capital Letters
+                                                                                    #Get your own Wordnik API key (I obviously cannot share mine publicly), this would allow a bigger range of words as you could use wordnik's definition function and not have to use Pydictionary limitations of definitions in the code(which occasionally cause the program to crash)
                                                                                     
--- Password Generator.py
+++ Password Generator.py
@@ -1,38 +1,35 @@
-import string
-import random 
+import string                                                              #this python libary contains things such as alphabet and other character lists
+import random                                                              #using random function from this libary to create the randomized variables from the string libary lists that will be used in the password  
 
 print("Welcome to the Password Generator Program made by Liam Finnie\n\n")
 
-howmanyletters = input("how many letters do you want in your password? ")
+howmanyletters = input("how many letters do you want in your password? ")   #converting strings into integers to allow easier variable manipulation
 howmanysymbols = input("how many symbols do you want in your password? ")
 howmanynumbers = input("how many numbers do you want in your password? ")
-
-#converting to integers to allow easier variable manipulation
 
 howmanyletters = int(howmanyletters)
 howmanysymbols = int(howmanysymbols)
 howmanynumbers = int(howmanynumbers)
 
-pwsize = howmanyletters + howmanysymbols + howmanynumbers
+pwsize = howmanyletters + howmanysymbols + howmanynumbers                    #determining password size to create a list that the randomized letters,numbers,symbols will go into
 
-#importing Lists from string Libary for random generation of variables from said lists (not hardcoding every possible letter,symbol,number)
 
-alphabet = list(string.ascii_letters)
+alphabet = list(string.ascii_letters)                                        #importing Lists from string libary for random generation of variables from said lists (not hardcoding every possible letter,symbol,number)
 numbers = list(range(0, 10))
 symbol = list(string.punctuation)
 
-#newletter = alphabet[random.randint(0, len(alphabet)-1)]     Code that pulls random item out of appropiate list that will be in while loops
+#newletter = alphabet[random.randint(0, len(alphabet)-1)]                    Code that pulls random item out of appropiate list that will be in while loops
 #newsymbol = symbol[random.randint(0, len(symbol)-1)]
 #newnumber = numbers[random.randint(0, len(numbers)-1)]
 
-password = ["null"] * pwsize #creating a list of null values with length of the asked for password to allow randomized placement of new variables replacing null values
+password = ["null"] * pwsize                                                 #creating a list of null values with length of the asked for password to allow randomized placement of new variables replacing null values
 
 
-l = 0                        #defining variables used in appropiate while loops
+l = 0                                                                        #defining variables used in appropiate while loops
 s = 0
 n = 0
 
-while l < howmanyletters: #generating amount of random letters required in password and randomizing positioning within password length, this is why a populated list of null values is created, so items in the list already have a predetermined placement to replace
+while l < howmanyletters:                                                    #generating amount of random letters required in password and randomizing positioning within password length, this is why a populated list of null values is created, so items in the list already have a predetermined placement to replace
     newletter = alphabet[random.randint(0, len(alphabet)-1)]
     placement = random.randint(0, pwsize-1)  
     if password[placement] == "null":
@@ -53,7 +50,7 @@ while n < howmanynumbers:
         password[placement] = newnumber
         n = n +1     
 
-pwstringconvert = [str(x) for (x) in password]   #converting list back to string
+pwstringconvert = [str(x) for (x) in password]                               #converting list back to string
 newpassword = ""
 newpassword = newpassword.join(pwstringconvert)
 
