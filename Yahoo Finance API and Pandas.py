131ba04901625b10be32c91ead56337089be9f18
IntuitiveOne <ituition@live.co.uk>
2023-03-15 18:49:37 +0200

    Cleaning up code a bit

--- Yahoo Finance API and Pandas.py
+++ Yahoo Finance API and Pandas.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import yfinance as yf #yahoo finance api
 
-start = "2013-01-01" #Defining Time Period                
+start = "2013-01-01" #defining time period for dataset          
 end   = "2023-01-31"
 
-symbol = "MSFT" #Choosing a stock to examine(we could put any stock here yahoo finance has access to)
+symbol = "MSFT" #choosing a stock to examine(we could put any stock here yahoo finance has access to)
 
 
 
-df = yf.download(symbol, start, end) # defining the dataframe to download and storing it into a csv file
+df = yf.download(symbol, start, end) #defining the dataframe to download and storing it into a csv file
 df
 df.to_csv("microsoft_data.csv")
 
@@ -16,10 +16,10 @@ msft_stock = pd.read_csv("microsoft_data.csv", parse_dates= ["Date"], index_col 
 
 
 
-print(msft_stock.head()) #Example Methods we could call with the pandas libary
+print(msft_stock.head()) #example function we could call with the pandas libary such as the head function(which by default pulls the first 5 rows of a tabular data we created using the pd.read_csv function)
 
 yearlystock = msft_stock.resample("Y").mean().round() #getting the average stock value of each year in our time frame
 
-#just a simple template to demonstrate how to access financial data from yahoo finance and using the pandas libary, there are infinite possibilities
+#just a simple template to demonstrate how to access financial data from yahoo finance and using the pandas libary to prepare for manipulate of the dataset, there are infinite possibilities
 
 
